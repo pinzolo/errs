a4436f356033e62415a08e499bf5d6d53f30d8ef
pinzolo <pinzolo@gmail.com>
2018-09-20 18:01:51 +0900

    Add unite errors

---
+++ unite.go
@@ -0,0 +1,68 @@
+package errz
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type withErrors struct {
+	errs []error
+}
+
+func (we *withErrors) Error() string {
+	buf := &bytes.Buffer{}
+	// nolint: gas
+	fmt.Fprintf(buf, "contains %d errors...\n", len(we.errs))
+	for i, err := range we.errs {
+		// nolint: gas
+		fmt.Fprintf(buf, "%d) %s\n", i+1, err.Error())
+	}
+	return buf.String()
+}
+
+func (we *withErrors) Errs() []error {
+	return we.errs
+}
+
+// Unite 2 errors to 1 error that having internal error list.
+// If both error are nil, Unite returns nil.
+// If either error is nil, Unite returns another error.
+func Unite(err1, err2 error) error {
+	if err1 == nil && err2 == nil {
+		return nil
+	}
+	if err1 == nil {
+		return err2
+	}
+	if err2 == nil {
+		return err1
+	}
+
+	errs1 := Errs(err1)
+	errs2 := Errs(err2)
+	errs := make([]error, len(errs1)+len(errs2))
+	for i, e := range errs1 {
+		errs[i] = e
+	}
+	for i, e := range errs2 {
+		errs[len(errs1)+i] = e
+	}
+	return &withErrors{errs}
+}
+
+// Errs returns error list in given error.
+// If given error is nil, Errs returns nil.
+// If given error does not have error list, Errs returns error list that contains given error itself only.
+func Errs(err error) []error {
+	if err == nil {
+		return nil
+	}
+
+	type errser interface {
+		Errs() []error
+	}
+	if errs, ok := err.(errser); ok {
+		return errs.Errs()
+	}
+	return []error{err}
+}
---
+++ unite_test.go
@@ -0,0 +1,79 @@
+package errz_test
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+
+	"github.com/pinzolo/errz"
+)
+
+func TestUnite(t *testing.T) {
+	err1 := errors.New("error1")
+	err2 := errors.New("error2")
+	err3 := errors.New("error3")
+	data := []struct {
+		err1 error
+		err2 error
+		msg  string
+		note string
+	}{
+		{nil, nil, "", "both nil"},
+		{err1, nil, "error1", "nil err2"},
+		{nil, err2, "error2", "nil err1"},
+		{err1, err2, "contains 2 errors...\n1) error1\n2) error2\n", "unite"},
+		{errz.Unite(err1, err2), err3, "contains 3 errors...\n1) error1\n2) error2\n3) error3\n", "nested"},
+	}
+	for _, d := range data {
+		t.Run(d.note, func(t *testing.T) {
+			err := errz.Unite(d.err1, d.err2)
+			if d.err1 == nil && d.err2 == nil {
+				if err != nil {
+					t.Fatal("should return nil when both errors are nil")
+				}
+				return
+			}
+			if err == nil {
+				t.Fatal("Unite should not return nil when either error is not nil")
+			}
+			if err.Error() != d.msg {
+				t.Errorf("Unite invalid error message: want %s, got %s", d.msg, err.Error())
+			}
+		})
+	}
+}
+
+func TestErrs(t *testing.T) {
+	err1 := errors.New("error1")
+	err2 := errors.New("error2")
+	err3 := errors.New("error3")
+	data := []struct {
+		err1 error
+		err2 error
+		errs []error
+		note string
+	}{
+		{nil, nil, nil, "both nil"},
+		{err1, nil, []error{err1}, "nil err2"},
+		{nil, err2, []error{err2}, "nil err1"},
+		{err1, err2, []error{err1, err2}, "unite"},
+		{errz.Unite(err1, err2), err3, []error{err1, err2, err3}, "nested"},
+	}
+	for _, d := range data {
+		t.Run(d.note, func(t *testing.T) {
+			err := errz.Unite(d.err1, d.err2)
+			if d.err1 == nil && d.err2 == nil {
+				if errz.Errs(err) != nil {
+					t.Fatal("Errs should return nil when both errors are nil")
+				}
+				return
+			}
+			if err == nil {
+				t.Fatal("Errs should not return nil when either error is not nil")
+			}
+			if !reflect.DeepEqual(errz.Errs(err), d.errs) {
+				t.Errorf("invalid error message: want %+v, got %+vs", d.errs, errz.Errs(err))
+			}
+		})
+	}
+}
